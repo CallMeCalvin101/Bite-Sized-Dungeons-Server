6b3d4022e1d0b1f4848c56b6c51d43ae1f35baca
Yuruki <me@yuruki.net>
2024-02-24 01:25:38 -0800

    Begin folder structure

---
+++ src/game_state_manager.js
@@ -0,0 +1,12 @@
+/** Methods to interact with GameStates */
+export default class GameStateManager {
+    /** Processes an Action onto the game state */
+    static processAction(action, state) {
+        // TODO: Create a standard data structure for actions
+        // TODO: Process the action and add a timestamp
+    }
+    /** Generates an AIAction for the given entity using the given game state */
+    static generateAIAction(id, state) {
+    }
+    // TODO: Maybe create methods for different types of actions/mechanics?
+}
